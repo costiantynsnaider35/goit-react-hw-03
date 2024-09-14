bc883aa6165923e95d01ed287f5e8589a30c47cb
Ваше ім'я <constantin161089@gmail.com>
2024-09-14 16:57:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
-import contactListData from "./ContactList/ContactList.json";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import "./App.css";
 import "modern-normalize";
 
